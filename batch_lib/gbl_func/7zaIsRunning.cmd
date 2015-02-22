75ecd5b17422370ff8af1b82b91ff7ca97f561d4
chemicalbliss <uniflare@gmail.com>
2015-02-22 20:47:06 +0000

    Teaser Tidyup

--- batch_lib/gbl_func/7zaIsRunning.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-call c:/batch_settings.cmd
-cd "%armapath%"
-tasklist /FI "IMAGENAME eq 7za.exe" 2>NUL | find /I /N "7za.exe">NUL
-if "%ERRORLEVEL%"=="0" (
-	echo.true
-) else (
-	echo.false
-)
